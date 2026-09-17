ff1fd2bf3e43586859750dcdcf35b3104e14c327
Fred <lehingfred@gmail.com>
2026-09-17 13:49:59 +0300

    Initial commit

---
+++ backend/src/main/java/ee/bcskoolitus/BcskoolitusApplication.java
@@ -0,0 +1,13 @@
+package ee.bcskoolitus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcskoolitusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcskoolitusApplication.class, args);
+	}
+
+}
