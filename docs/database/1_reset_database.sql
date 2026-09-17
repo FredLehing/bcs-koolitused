e99dcf8a6d247239eaf02d6a9dc39881b8311cca
Fred <lehingfred@gmail.com>
2026-09-17 14:21:41 +0300

    db changes

--- docs/database/1_reset_database.sql
+++ docs/database/1_reset_database.sql
@@ -1,7 +1,7 @@
 -- Kustutab minu_projekt schema (mis põhimõtteliselt kustutab kõik tabelid)
-DROP SCHEMA IF EXISTS minu_projekt CASCADE;
+DROP SCHEMA IF EXISTS bcs_koolitused CASCADE;
 -- Loob uue minu_projekt schema vajalikud õigused
-CREATE SCHEMA minu_projekt
+CREATE SCHEMA bcs_koolitused
 -- taastab vajalikud andmebaasi õigused
-    GRANT ALL ON SCHEMA minu_projekt TO postgres;
-GRANT ALL ON SCHEMA minu_projekt TO PUBLIC;
+    GRANT ALL ON SCHEMA bcs_koolitused TO postgres;
+GRANT ALL ON SCHEMA bcs_koolitused TO PUBLIC;
