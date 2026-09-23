824f5fa0b1fbef7b0b1c7c0ec10daabee345b4e8
Fred <lehingfred@gmail.com>
2026-09-23 15:04:27 +0300

    Login Service make it pretty

--- backend/src/main/java/ee/bcskoolitus/service/LoginService.java
+++ backend/src/main/java/ee/bcskoolitus/service/LoginService.java
@@ -33,11 +33,19 @@ public class LoginService {
         User user = userRepository.findUserBy(loginRequest.getEmail(), loginRequest.getPassword(), ApiStatus.STATUS_ACTIVE.getCode())
                 .orElseThrow(() -> new ForbiddenException(INCORRECT_CREDENTIALS.getMessage(), INCORRECT_CREDENTIALS.name()));
         LoginResponse loginResponse = userMapper.toLoginResponse(user);
+        handleSetSystemLanguages(user, loginResponse);
+        return loginResponse;
+    }
+
+    private void handleSetSystemLanguages(User user, LoginResponse loginResponse) {
         if (ROLE_ADMIN.getName().equals(user.getRole().getName())) {
-            List<Language> languages = languageRepository.findAll();
-            List<SystemLanguageDto> systemLanguageDtos = languageMapper.toSystemLanguageDtos(languages);
+            List<SystemLanguageDto> systemLanguageDtos = getSystemLanguageDtos();
             loginResponse.setSystemLanguages(systemLanguageDtos);
         }
-        return loginResponse;
+    }
+
+    private List<SystemLanguageDto> getSystemLanguageDtos() {
+        List<Language> languages = languageRepository.findAll();
+        return languageMapper.toSystemLanguageDtos(languages);
     }
 }
