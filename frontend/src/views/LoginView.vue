d9871a4a93ba2ceae65c16983a9d1b894e20ed5d
evelyki-a11y <evelyki@gmail.com>
2026-09-24 00:10:34 +0300

    LoginView_wip2

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,17 +1,56 @@
 <template>
-  <div class="container text-center">
-    <div class="alert alert-primary" role="alert">Sisselogimine
+  <div class="container">
+    <div class="row">
+      <div class="col-6"></div>
 
+      <div class="col-6 text-center">
+        <h1>Sisselogimine</h1>
+        <div v-if="errorMessage" class="alert alert-danger">{{ errorMessage }}</div>
 
+        <div class="row mb-3">
+          <label class="col-3 col-form-label">Email</label>
+          <div class="col-6">
+            <input v-model="email" type="email" class="form-control" />
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <label class="col-3 col-form-label">Parool</label>
+          <div class="col-6">
+            <input v-model="password" type="password" class="form-control" />
+          </div>
+        </div>
+        <div class="form-check mb-3 d-inline-block">
+          <input type="checkbox" class="form-check-input" id="rememberMe" />
+          <label class="form-check-label" for="rememberMe">Jäta mind meelde</label>
+        </div>
+        <div></div>
+        <button @click="login" type="button" class="btn btn-primary">Logi sisse</button>
+
+        <div class="mt-2">
+          <a href="#">Unustasid salasõna?</a>
+        </div>
+      </div>
     </div>
-
   </div>
 </template>
 
 <script>
-
 export default {
-
-
+  name: 'LoginView',
+  data() {
+    return {
+      email: '',
+      password: '',
+      errorMessage: '',
+    }
+  },
+  methods: {
+    login() {
+      if (this.email === '' || this.password === '') {
+        this.errorMessage = 'Täida kõik väljad'
+      }
+    },
+  },
 }
 </script>
