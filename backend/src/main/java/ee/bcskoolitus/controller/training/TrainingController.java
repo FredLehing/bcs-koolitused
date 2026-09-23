f1ffcc759c777763a7d49de0b2bdddea895c2eff
Fred Lehing <lehingfred@gmail.com>
2026-09-23 20:48:14 +0300

    TrainingsView backend wip

--- backend/src/main/java/ee/bcskoolitus/controller/training/TrainingController.java
+++ backend/src/main/java/ee/bcskoolitus/controller/training/TrainingController.java
@@ -1,5 +1,6 @@
 package ee.bcskoolitus.controller.training;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TrainingController {
     @GetMapping("/trainings")
+    @Operation(summary = "Tagastab koolituste nimekirja koos leheküljestamiseks vajaliku metainfoga")
     public void getTrainings(@RequestParam(required = false) Integer categoryId,
                              @RequestParam(required = false) Integer fundingTypeId,
                              @RequestParam(defaultValue = "5") Integer limit,
