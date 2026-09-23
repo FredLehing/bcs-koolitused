8b79849203ef8a41336a60d41549daa8c8c4d567
evelyki-a11y <evelyki@gmail.com>
2026-09-23 16:03:22 +0300

    LoginView_wip1

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container text-center">
+    <div class="alert alert-primary" role="alert">Sisselogimine
+
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+
+
+}
+</script>
