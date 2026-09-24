6b1bd720b4903b6def294b3c95f45d8b9b3d1818
evelyki-a11y <evelyki@gmail.com>
2026-09-24 22:16:32 +0300

    LoginView_sisselogimine asukoht muudetud

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container flex-grow-1 d-flex flex-column justify-content-center">
     <div class="row">
       <div class="col-6"></div>
 
@@ -7,19 +7,27 @@
         <h1>Sisselogimine</h1>
         <div v-if="errorMessage" class="alert alert-danger">{{ errorMessage }}</div>
 
-        <div class="row mb-3">
-          <label class="col-3 col-form-label">Email</label>
-          <div class="col-6">
-            <input v-model="email" type="email" class="form-control" />
-          </div>
+        <div class="form-floating mb-3 w-75 mx-auto">
+          <input
+            v-model="email"
+            type="email"
+            class="form-control"
+            id="floatingInput"
+            placeholder="Email"
+          />
+          <label for="floatingInput">Email</label>
+        </div>
+        <div class="form-floating mb-3 w-75 mx-auto">
+          <input
+            v-model="password"
+            type="password"
+            class="form-control"
+            id="floatingPassword"
+            placeholder="Parool"
+          />
+          <label for="floatingPassword">Parool</label>
         </div>
 
-        <div class="row mb-3">
-          <label class="col-3 col-form-label">Parool</label>
-          <div class="col-6">
-            <input v-model="password" type="password" class="form-control" />
-          </div>
-        </div>
         <div class="form-check mb-3 d-inline-block">
           <input type="checkbox" class="form-check-input" id="rememberMe" />
           <label class="form-check-label" for="rememberMe">Jäta mind meelde</label>
